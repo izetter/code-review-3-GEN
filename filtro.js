9844f1d6113b2466e5df648433c089a07be24070
izetter <59151193+izetter@users.noreply.github.com>
2023-09-01 10:48:49 -0600

    end

--- filtro.js
+++ filtro.js
@@ -8,58 +8,69 @@ const productos = [
 	{ nombre: 'Zapato rojo', tipo: 'zapato', color: 'rojo', img: './zapato-rojo.jpg' },
 ];
 
-const li = document.getElementsByName('lista-de-productos');
-const $i = document.querySelector('.input');
+// change variabe names to meaningful names throughout the file
+// ideally use english for variable names, HTML attrinbutes etc.
 
-for (let i = 0; i < productos.length; i++) {
-	var d = document.createElement('div');
-	d.classList.add('producto');
+// use selectors that makes sense
+const productList = document.getElementById('lista-de-productos');
+const input = document.querySelector('input');
 
-	var ti = document.createElement('p');
-	ti.classList.add('titulo');
-	ti.textContent = productos[i].nombre;
-
-	var imagen = document.createElement('img');
-	imagen.setAttribute('src', productos[i].img);
-
-	d.appendChild(ti);
-	d.appendChild(imagen);
-
-	li.appendChild(d);
-}
-
-displayProductos(productos);
-const botonDeFiltro = document.querySelector('button');
-
-botonDeFiltro.onclick = function () {
-	while (li.firstChild) {
-		li.removeChild(li.firstChild);
-	}
-
-	const texto = $i.value;
-	console.log(texto);
-	const productosFiltrados = filtrado(productos, texto);
-
-	for (let i = 0; i < productosFiltrados.length; i++) {
-		var d = document.createElement('div');
-		d.classList.add('producto');
-
-		var ti = document.createElement('p');
-		ti.classList.add('titulo');
-		ti.textContent = productosFiltrados[i].nombre;
-
-		var imagen = document.createElement('img');
-		imagen.setAttribute('src', productosFiltrados[i].img);
-
-		d.appendChild(ti);
-		d.appendChild(imagen);
-
-		li.appendChild(d);
-	}
-};
-
+// Prefer not to rely on hoisting. Moved this func higher
 const filtrado = (productos = [], texto) => {
 	return productos.filter(
 		(item) => item.tipo.includes(texto) || item.color.includes(texto)
 	);
 };
+
+for (let i = 0; i < productos.length; i++) {
+
+	// prefer const and let over var
+	// change variabe names to meaningful names
+	const div = document.createElement('div');
+	div.classList.add('producto');
+
+	const p = document.createElement('p');
+	p.classList.add('titulo');
+	p.textContent = productos[i].nombre;
+
+	const img = document.createElement('img');
+	img.setAttribute('src', productos[i].img);
+
+	div.appendChild(p);
+	div.appendChild(img);
+
+	productList.appendChild(div);
+}
+
+// displayProductos(productos);
+const botonDeFiltro = document.querySelector('button');
+
+botonDeFiltro.onclick = function () {
+	while (productList.firstChild) {
+		productList.removeChild(productList.firstChild);
+	}
+
+	const texto = input.value;
+	console.log(texto);
+	const productosFiltrados = filtrado(productos, texto);
+
+	// prefer const and let over var
+	// change variabe names to meaningful names
+	for (let i = 0; i < productosFiltrados.length; i++) {
+		const div = document.createElement('div');
+		div.classList.add('producto');
+
+		const p = document.createElement('p');
+		p.classList.add('titulo');
+		p.textContent = productosFiltrados[i].nombre;
+
+		const img = document.createElement('img');
+		img.setAttribute('src', productosFiltrados[i].img);
+
+		div.appendChild(p);
+		div.appendChild(img);
+
+		productList.appendChild(div);
+	}
+};
+
